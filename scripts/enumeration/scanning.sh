736a52cb3836d464559cd0512c4a235efde2c4a3
Glitch-Gecko <nvformella@gmail.com>
2024-02-07 17:48:32 -0600

    Improved scan script

--- scripts/enumeration/scanning.sh
+++ scripts/enumeration/scanning.sh
@@ -6,7 +6,7 @@ then
     # insert nmap command
 else
     echo "Running rustscan"
-    ports=$(rustscan -a 'hosts.txt' --ulimit 5000 -g | grep -Po '(?<=\[).*(?=\])'| tr '\n' ','| sed 's/.$//')
+    ports=$(rustscan -a 'hosts.a' --ulimit 5000 -g | grep -Po '(?<=\[).*(?=\])'| tr '\n' ','| sed 's/.$//')
     echo "Open ports: $ports"
-    sudo rustscan -a 'hosts.txt' --ulimit 5000 --ports $ports -- -sV -g 53 -Pn --disable-arp-ping -A -oA scan_results
+    sudo rustscan -a 'hosts.a' --ulimit 5000 --ports $ports -- -sV -g 53 -Pn --disable-arp-ping -A > scan_results
 fi
